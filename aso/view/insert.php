d8f85d0f8224114d078dcfaa850627b4fe0dc502
hayashi <st1201515@yahoo.co.jp>
2014-11-19 13:00:48 +0900

    syuusei

--- aso/view/insert.php
+++ aso/view/insert.php
@@ -7,7 +7,7 @@
 		$mid = $_POST['m_id'];
 		//SQL文の格納
 		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query = "update vote set m_votes = m_votes + 1 where m_id = " .$mid.")";
+		$query = "update vote set m_votes = m_votes + 1 where m_id = " .$mid." and  j_id  = " . $_SESSION['select_j'])";
 		$result = mysqli_query($dbc, $query);
 		
 

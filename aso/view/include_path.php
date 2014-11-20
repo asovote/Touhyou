9dfef67c788d6588d44f7f42679bbb9e5dc30ab6
Kurogi <st1201515@yahoo.co.jp>
2014-11-20 14:28:18 +0900

    ああああああ

--- aso/view/include_path.php
+++ aso/view/include_path.php
@@ -1,4 +1,8 @@
 <?php
 	//インクルードのパス設定
+<<<<<<< HEAD
 	ini_set('include_path', __DIR__ . '/../class/');
+=======
+	ini_set('include_path', __DIR__ .'/../class/');
+>>>>>>> 4c9c1fe5465011d760747e1ee4121c4383f793e0
 ?>

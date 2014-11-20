5de627dbae458da869027a87ef76423355e4da0e
Kurogi <st1201515@yahoo.co.jp>
2014-11-20 15:08:06 +0900

    二重投票対策

--- aso/view/ok.php
+++ aso/view/ok.php
@@ -1,4 +1,6 @@
-﻿<!doctype html>
+﻿
+
+<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -7,14 +9,15 @@
 <body>
 <?php
 
-	$id =$_POST['$mid'];
-	if($mid!="a"){
-	echo '本当に投票してもよろしいですか？';
-	echo '<a href="ok.php?mid=a" ></a>';
-	}
-	else{
-	echo '投票ありがとうございました';
-	}
+        $jid = $_SESSION['$janru']; //スレッドID
+	
+ 	if(isset($_COOKIE[$id])){ 
+ 	print("連続投票です。"); 
+	exit; 
+	 }else{
+	echo '投票ありがとうございました'; 
+	setcookie($jid, "vete_flg", time()+$3600*24*7); 
+	 } 
  ?>
 </body>
 </html>

a644c07d63df7f6095d3dfb796c4db51fcf8a95f
hayashi <st1201515@yahoo.co.jp>
2014-12-01 18:55:33 +0900

    syusei

--- aso/view/select_janru.php
+++ aso/view/select_janru.php
@@ -3,20 +3,27 @@
 ジャンル選択
 </title>
 <head>
+<link type="text/css" rel="stylesheet" href="./select_janru.css">
+
 </head>
 <body>
+<body style="background-image:url(背景2.png);background-attachment:fixed;">
 
 <?php
+
+		echo '<div id="link"><p><a href="kanri_top.html">トップへ戻る</a></p></div>';
+		echo '<div id="top"><p1>投票結果画面</p1></div>';
+
 	session_start();
-	ini_set( "display_errors", "Off");
-	//require("http://enzerus.com/aso/ix/atack.php");
+	//ini_set( "display_errors", "Off");
+	//require("http://enzerus.com/aso/atack.php");
 	
 	$mysqli = new mysqli('localhost', 'root', '');
 	if ($mysqli -> connect_errno) {
 		print('<p>データベースへの接続に失敗しました。</p>' . $mysqli -> connect_error);
 		exit();
 	}
-	
+	//
 	$mysqli -> select_db('test');
 	$mysqli -> set_charset("utf-8");
 	
@@ -30,7 +37,7 @@
 	}
 	
 	$k = "<br/>";
-	
+	echo '<div id="button">';
 	while($row = $janru_list -> fetch_assoc()) {
 		$j_id = $row['j_id'];
 		$j_name = $row['j_name'];
@@ -40,8 +47,7 @@
 		printf($bun1);
 		printf($bun2,$j_id,$j_name);
 	}
-		echo '<p><a href="kanri_top.html">トップへ戻る</a>';
-
+	echo '</div>';
 
 ?>
 

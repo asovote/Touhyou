321d5ab5753eecea126ae25b680a153f656474e9
れがろ <st1201515@yahoo.co.jp>
2014-12-03 03:57:07 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -49,7 +49,6 @@
 		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
 		$query = "select * from janru,mj_list,member where member.m_id = mj_list.m_id and mj_list.j_id = janru.j_id and janru.j_id=".$janru.";";
 		$result = mysqli_query($dbc, $query);
-		echo $query;
 
 		while($row = mysqli_fetch_array($result)){
 			

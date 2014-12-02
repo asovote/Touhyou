2feaf4773c006ab26ceae4176afa1a33e37a6f2a
れがろ <st1201515@yahoo.co.jp>
2014-12-03 07:50:22 +0900

    no message

--- aso/view/update.php
+++ aso/view/update.php
@@ -1,6 +1,7 @@
 <?php
 		
 		session_start();
+		$_SESSION['jid'] = $_POST['jid'];
 		//データベースにつなぐ
 		require_once('include_path.php');
 		require_once('db.php');
@@ -8,7 +9,7 @@
 		
 		//SQL文の格納
 		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query ="update mj_list set votes = votes + 1 where m_id =".$_POST['mid']." and j_id =".$_POST['mid'].";";
+		$query ="update mj_list set votes = votes + 1 where m_id =".$_POST['mid']." and j_id =".$_POST['jid'].";";
 		$result = mysqli_query($dbc, $query);
 		
 

caa71c17b93b0f95764578bf52ca87e993dd5dfe
れがろ <st1201515@yahoo.co.jp>
2014-12-03 05:50:46 +0900

    no message

--- aso/view/u_profile_select.php
+++ aso/view/u_profile_select.php
@@ -1,70 +1,36 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>プロフィール編集画面</title>
-</head>
-	
-<body>
-
-
-	<?php
-		//データベースに接続
-		require_once('include_path.php');
-		require_once('db.php');
-		require_once('session_start.php');
-	
-		if(isset($_GET['jid'])){
-		$j_id = $_GET['jid'];
-		}else if(isset($_GET['mid'])){
-		$mid = $_GET['mid'];
-		}
-		
-		echo '<p>参加者情報参照画面</p>';
-		
-		
-	if(isset($mid)){
-	
-		//トップ画面へのリンク
-		echo '<p><a href="usertop.php">トップへ戻る</a>';
-		
-	
-		//SQL文の格納
-		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query = "select * from janru,mj_list,member where member.m_id = mj_list.m_id and mj_list.j_id = janru.j_id and member.m_id=".$mid.";";
-		$result = mysqli_query($dbc, $query);
-		
-	if(mysqli_num_rows($result) == 0) {
-			//該当する商品が見つからない場合
-			echo '<p>該当する人物が見つかりませんでした。</p>';
-	} else {
-		require_once('session_out.php');
-		// 取得したデータを一覧表示
-		while($row = mysqli_fetch_array($result)){
-			
-			//表示処理
-			$mid = $row['m_id'];
-			$mname = $row['name'];
-			$mschool = $row['school'];
-			$mjanru = $row['j_name'];
-			$mfree = $row['free'];
-			echo '<a href="u_profile_select.php?mid=' .$mid.'" ><p>'.$mname.'</p></a>';
-			echo '<p>名前：'.$mname.'</p>';
-			echo '<p>学校：'.$mschool.'</p>';
-			echo '<p>ジャンル：'.$mjanru.'</p>';
-			echo '<p>フリーワード：'.$mfree.'</p>';
-			
-		}
-	}
-	}else if(isset($jid)){
-	
-
-
-
-	}
-		
-
-	?>
-</body>
+﻿<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Title</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
+    
+	<body>
+  
+      <div class="container">
+      
+    <h1>登録者詳細</h1>
+    <div class="row"><!--実際に使う際はここをループさせて表示します-->        
+        
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350"></a></div><!--SQLで撮ってきた画像に差し替え-->
+      <div class="col-lg-3 col-sm-4 col-xs-6">name
+            <div align="center" valign="bottom">戻る　投票
+            </div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>
+        </div>   <div class="row"><!--実際に使う際はここをループさせて表示します-->
+          </div>
+          </div>
+        
+        <div class="container">
+        ここで個人の詳細を・・・
+        </div>
+	</body>
+    
 </html>

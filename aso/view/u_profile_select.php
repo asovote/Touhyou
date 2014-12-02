5313b830b83322698ef824ad3c8160c003399f82
れがろ <st1201515@yahoo.co.jp>
2014-12-03 05:58:02 +0900

    no message

--- aso/view/u_profile_select.php
+++ aso/view/u_profile_select.php
@@ -14,15 +14,15 @@
 	</head>
     
 	<body>
-  
+  <body style="background-image:url(背景2.png);background-attachment:fixed;">
       <div class="container">
       
-    <h1 align="center">登録者詳細</h1>
-    <div class="row"><!--実際に使う際はここをループさせて表示します-->        
+    <h1 align="center">出演者詳細</h1>
+    <div class="row">        
         
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350"></a></div><!--SQLで撮ってきた画像に差し替え-->
       <div class="col-lg-3 col-sm-4 col-xs-6">name
-            <div align="center" valign="bottom">戻る　投票
+            <div align="center" valign="bottom"><input type="button" value="戻る" onclick="history.back()"><input type="submit"value="投票">
             </div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>
         </div>   <div class="row"><!--実際に使う際はここをループさせて表示します-->
           </div>

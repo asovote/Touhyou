11740c1e199b267c10d3c9de6732b0b0d320031c
れがろ <st1201515@yahoo.co.jp>
2014-12-03 08:13:24 +0900

    no message

--- aso/view/update.php
+++ aso/view/update.php
@@ -11,7 +11,7 @@
 		$query ="update mj_list set votes = votes + 1 where m_id =".$_POST['mid']." and j_id =".$_POST['jid'].";";
 		$result = mysqli_query($dbc, $query);
 		
-		$_SESSION['jid'];
+		 echo $_SESSION['jid'];
 
 //header('Location:ok.php');
 

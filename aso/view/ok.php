7224516b2eb4859d17eb2dbd15b7b02819fcd022
れがろ <st1201515@yahoo.co.jp>
2014-12-03 07:38:36 +0900

    no message

--- aso/view/ok.php
+++ aso/view/ok.php
@@ -12,13 +12,7 @@
         $jid = $_SESSION['$jid']; //スレッドID
 	
 	echo $jid;
- 	if(isset($_COOKIE[$id])){ 
- 	print("連続投票です。"); 
-	exit; 
-	 }else{
-	echo '投票ありがとうございました'; 
-	setcookie($jid, "vete_flg", time()+$3600*24*7); 
-	 } 
+
  ?>
 </body>
 </html>

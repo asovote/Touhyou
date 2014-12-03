4fe24147b4f19528d9bc10a93eed625cad211a7b
hayashi <st1201515@yahoo.co.jp>
2014-12-03 10:09:18 +0900

    asd

--- aso/view/vote_manage.php
+++ aso/view/vote_manage.php
@@ -11,11 +11,13 @@
 </form>
 
 <?php
+
+session_start();
+
 if($_SESSION['ad_id'] == null){
 header('Location: /ad_login.php');
 }
 
-	session_start();
 	$k = "<br/>";
 //	$get_janru = $_POST['select_j'];  //POSTで送られたjanruID取得
 //	printf('<form method="POST" action = "../e_m_e_search">');

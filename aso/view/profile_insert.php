e4f2beaa5e8ca87f7cf5e2095455cd0bb63050ef
hayashi <st1201515@yahoo.co.jp>
2014-12-03 10:41:49 +0900

    asd

--- aso/view/profile_insert.php
+++ aso/view/profile_insert.php
@@ -10,10 +10,12 @@
 <Div Align="center">
 
 	<?php
-session_start();
-if($_SESSION['ad_id'] == null){
-header('Location: /ad_login.php');
-}
+
+
+		session_start();
+		if($_SESSION['ad_id'] == null){
+		header('Location: /ad_login.php');
+		}
 
 		//データベースに接続
 	//	ini_set('include_path', '/xampp/htdocs/aso/classes/');

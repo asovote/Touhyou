3d121fafd20f73d43051ca13824a057473423a5d
Kurogi <st1201515@yahoo.co.jp>
2014-12-03 11:14:29 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -55,9 +55,7 @@
 			$jid = $row['j_id'];
  			$mimg = $row['m_img'];
 			$_SESSION['jid'] = $jid;
-			?>
-            <!--実際に使う際はここをループさせて表示します-->
-<?php 
+
           echo'<div class="row">';
 		  echo '<form action="update.php" method="POST">';
       		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';

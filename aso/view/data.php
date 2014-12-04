697def1a3a08b8acc2d3b01317a83bf91ae1fece
Kurogi <st1201515@yahoo.co.jp>
2014-12-04 14:54:47 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -60,9 +60,12 @@ if(isset($_COOKIE[$jid])){
  			$mimg = $row['m_img'];
 			$_SESSION['jid'] = $jid;
          	  echo'<div class="row">';
+		  echo '<form action="update.php" method="POST">';
       		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">'; echo'<h3>'.$mname.'</h3>';
-		  echo'</div>'; 	
+		  echo'<div align="center" valign="bottom">';
+                  echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
+		  echo'</div></div>';
 }
 }else{
 		//データベースにつなぐ

b535525b1b2c3a0d95d7b6b7c46961c1fd331a0d
1201519 <st1201515@yahoo.co.jp>
2014-12-05 12:09:31 +0900

    ok.phpの配置、サイズ変更

--- aso/view/ok.php
+++ aso/view/ok.php
@@ -10,15 +10,17 @@
 <?php
 session_start();	
 $jid = $_SESSION['jid']; //スレッドID
-
+echo '<center>';
+echo '<p><font size="80px">';
 if(isset($_COOKIE[$jid])){ 
  	print("連続投票です。"); 
 	exit; 
 	 }else{
-	echo '投票ありがとうございました'; 
+	echo '<br><br>投票ありがとうございました'; 
 	setcookie($jid,"vote_flag",time()+3600*24*7,"/"); 
 	 } 
-
+echo '</font></p>';
+echo '</center>';
  ?>
 </body>
 </html>

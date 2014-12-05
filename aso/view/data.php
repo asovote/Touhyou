434303937d56e0d34f7409ce451d693d7cb3bd5b
Kurogi <st1201515@yahoo.co.jp>
2014-12-05 12:29:37 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -28,7 +28,7 @@
 <body style="background-image:url(背景2.png);background-attachment:fixed;">
 <div class="container">
 <h1 align=center>出演者一覧</h1>
-<input type="button" value="戻る" onclick="history.back()">
+<input type="button" value="戻る" onclick="history.back()"></br>
 <?php	
 		require_once('include_path.php');
 		require_once('db.php');
@@ -91,7 +91,7 @@ if(isset($_COOKIE[$jid])){
       		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">'; echo'<h3>'.$mname.'</h3>';
-		  echo'<div align="center" valign="bottom"><input type="submit"value="投票" "><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form>';
+		  echo'<div align="center" valign="bottom"><input type="submit"value="投票" "><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form><br>';
                   echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
 		  echo'</div></div>';
 	}	 

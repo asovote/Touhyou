d741077ede8a59faa47225b55a72ffd471bf48e1
1201503 <st1201515@yahoo.co.jp>
2014-12-10 13:22:46 +0900

    a

--- aso/view/user_result.php
+++ aso/view/user_result.php
@@ -1,137 +1,149 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
-		<title>Title</title>
-		<meta name="generator" content="Bootply" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<link href="css/styles.css" rel="stylesheet">
-
-				
-	<script type="text/javascript">
-	    /**
-	     * 確認ダイアログの返り値によりフォーム送信
-	    */
-	    function submitChk() {
-	        /* 確認ダイアログ表示 */
-	        var flag = confirm ( "投票してもよろしいですか？");
-	        /* send_flg が TRUEなら送信、FALSEなら送信しない */
-	        return flag;
-	    }
-	</script>
-	</head>  
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>全件表示</title>
+</head>
+<body>
 <body style="background-image:url(背景2.png);background-attachment:fixed;">
-<?php /*
-class SimpleClass
-{
-public $m_id;
-public $name;
-public $j_id;
-public $m_img;
-}*/
-?>
-<div class="container">
-<h1 align=center>出演者一覧</h1>
-<input type="button" value="戻る" onclick="history.back()"></br>
-<?php	
-		require_once('include_path.php');
-		require_once('db.php');
-		require_once('session_start.php');
-		header('Expires:-1');
-		header('Cache-Control:');
-		header('Pragma:');
+
+<form method ="POST" action = "select_janru.php">
+<input type ="submit" value="戻る">
+</form>
+
+<?php
+	
+	session_start();
+	$k = "<br/>";
+//	$get_janru = $_POST['select_j'];  //POSTで送られたjanruID取得
+//	printf('<form method="POST" action = "../e_m_e_search">');
+//	printf('<input type="submit" value="投票数変更" onClick="form.action=\'vote_edit.php\';return true"/>');
+	
+	
+	
+	if(isset($_POST['jid'])){
+	$select_j_id = $_POST['jid']; //スレッドID
+	$_SESSION['jid']=$select_j_id;
+	}else if($_SESSION['jid']){
+	$select_j_id=$_SESSION['jid'];
+	}else{
+	echo '値が入っていません';
+	}
+
+	
+	
+/*	
+	if(!isset($_POST['select_j'])) {
+	$select_j_id = $_SESSION['select_j'];
+	}else{
+	$select_j_id = $_POST['select_j'];
+	$_SESSION['select_j'] =$select_j_id;
+	}
+	
+*/	
 
 
-if(isset($_POST['jid'])){
-$jid = $_POST['jid']; //スレッドID
-$_SESSION['jid']=$jid;
-}else if($_SESSION['jid']){
-$jid=$_SESSION['jid'];
-}else{
-echo '値が入っていません';
-}
+	
+	require_once('include_path.php');
+	require_once('db.php');
+	$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
 
-if(isset($_COOKIE[$jid])){ 
-		//データベースにつなぐ
-		if(isset($_POST['jid'])){
-		$janru = $_POST['jid'];
-		}else{
-		header("Location: usertop.php");
-		}
-		//SQL文の格納
-		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query = "select * from janru,mj_list,member where member.m_id = mj_list.m_id and mj_list.j_id = janru.j_id and janru.j_id=".$janru."order by votes desc ";
-		$result = mysqli_query($dbc, $query);
+	
+	printf($k);printf($k);printf($k);
+	
+	printf($k);printf($k);
+	
+	
+	$mj_list_query = "select * from mj_list where j_id = " . $select_j_id; // . "order by m_id"
+//	選択されたジャンルに参加する人のm_idを取得する
+	
+	$mj_list_result = $dbc -> query($mj_list_query);
+	while($mj_list_row = $mj_list_result -> fetch_assoc()) {
+	
+//	ジャンルの参加人数分だけ繰り返す
+//	参加者の情報を1人ずつ表示する
+
+//		$bun1 = "参加者ID:%d "; // ""内はHTML
+		$m_id = $mj_list_row['m_id'];
 		
-	        //SimpleClassのリストを宣言　sList
-	       
-		while($row = mysqli_fetch_array($result)){
 		
-/*		     //SimpleClass を　new する　変数名：sc
-		     //scの$m_idに$row['m_id']を入れる
-		     //・・
-		     //sListにscを追加
-		 //}
-		 
-		 //ｓListにシャッフルをする
-		 
-		 //for　0番目から、配列の件数までループ
-		 //｛
-		 　　//sListのi番目を表示
-		 
-		 //｝
-*/			
-			//表示処理
-			$mid = $row['m_id'];
-			$mname = $row['name'];
-			$jid = $row['j_id'];
- 			$mimg = $row['m_img'];
-			$_SESSION['jid'] = $jid;
-         	  echo'<div class="row">';
-		  echo '<form action="update.php" method="POST">';
-      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="un_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
-		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">'; echo'<h4>'.$mname.'</h4>';
-		  echo'<div align="center" valign="bottom">';
-                  echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
-		  echo'</div>';
-		}
-}else{
-		//データベースにつなぐ
+		$query = "select * from member where m_id =" . $m_id; //とってきたジャンルで選択されたmemberを一人ずつ表示
+		$result = $dbc -> query($query);
 		
-		if(isset($_POST['jid'])){
-		$janru = $_POST['jid'];
-		}else{
-		header("Location: usertop.php");
+		if(!$result){
+			printf('query failed.' . $dbc -> error);
+			$dbc -> close();
+			exit();
 		}
-		//SQL文の格納
-		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query = "select * from janru,mj_list,member where member.m_id = mj_list.m_id and mj_list.j_id = janru.j_id and janru.j_id=".$janru."order by votes desc ";
-		$result = mysqli_query($dbc, $query);
-	       
-	       
-	       
-		while($row = mysqli_fetch_array($result)){
+		$k = "<br/>";
+		
+		$query = "select * from member,mj_list,janru where member.m_id =".$m_id." and mj_list.m_id = member.m_id and mj_list.j_id = janru.j_id order by mj_list.votes desc"; //とってきたジャンルで選択されたmemberを一人ずつ表示
+		$result = $dbc -> query($query);
+					
+		while($row = $result -> fetch_assoc()) {
 			
-			//表示処理
-			$mid = $row['m_id'];
-			$mname = $row['name'];
-			$jid = $row['j_id'];
- 			$mimg = $row['m_img'];
-			$_SESSION['jid'] = $jid;
-         	  echo'<div class="row">';
-      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
-		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';
-		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">'; echo'<h3>'.$mname.'</h3>';
-		  echo'<div align="center" valign="bottom"><input type="submit"value="投票" "><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form><br>';
-                  echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
-		  echo'</div>';
-		}	 
-}
-?>   </div> </body>
-    
+			
+			$bun1 = "参加者ID:%d "; // ""内はHTML
+			$img_m_id = $row['m_id'];
+			printf($bun1,$img_m_id);
+			printf($k);
+			
+			$bun2 = "参加者名:%s ";
+			$m_name = $row['name'];
+			printf($bun2,$m_name);
+			printf($k);
+
+
+
+//			$img_query = "SELECT IMG FROM member WHERE m_id = " . $m_id ;
+//			$img_result = $mysqli -> query($img_query);
+//			$img_row = $img_result -> fetch_row();
+//			echo "<img src=" . $img_row[0] . ">";
+//			printf($img);
+//			printf($k);
+//			→直接表示すると文字化けする。
+
+
+			require('imgget.php');		
+		
+		
+			$bun3 = "参加者紹介文:%s";
+			$free = $row['free'];
+			printf($bun3,$free);
+			printf($k);
+			
+			
+			$vquery = "select * from mj_list where m_id =". $m_id . " and j_id = " . $_SESSION['select_j'];
+			$vresult = $dbc -> query($vquery);
+			while($vote_row = $vresult -> fetch_assoc()) {
+			
+				$total = $vote_row['votes'];
+				printf("<br />"."得票数: %d \n", $total);
+					
+			}
+			
+			
+			printf($k);printf($k);printf($k);printf($k);printf($k);
+			printf("----------------------------------------------------------------------------------------------");
+			printf($k);printf($k);printf($k);printf($k);printf($k);
+		}
+	}
+	
+	
+	
+	printf($k);printf($k);printf($k);
+	printf($k);printf($k);printf($k);
+	
+	
+	
+
+	
+		
+		
+	
+//	unset($_SESSION['img_id']);
+
+?>
+
+<html>
+</body>
 </html>

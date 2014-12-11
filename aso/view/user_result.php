78aeeca9446252d9606e58002e7ec11c4c1b9b8f
1201503 <st1201515@yahoo.co.jp>
2014-12-12 03:37:28 +0900

    a

--- aso/view/user_result.php
+++ aso/view/user_result.php
@@ -1,7 +1,7 @@
 ﻿<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>全件表示</title>
+<title>ランキング</title>
 </head>
 <body>
 <body style="background-image:url(背景2.png);background-attachment:fixed;">
@@ -13,12 +13,7 @@
 <?php
 	
 	session_start();
-	$k = "<br/>";
-//	$get_janru = $_POST['select_j'];  //POSTで送られたjanruID取得
-//	printf('<form method="POST" action = "../e_m_e_search">');
-//	printf('<input type="submit" value="投票数変更" onClick="form.action=\'vote_edit.php\';return true"/>');
-	
-	
+	$k = "<br/>";	
 	
 	if(isset($_POST['jid'])){
 	$select_j_id = $_POST['jid']; //スレッドID
@@ -29,29 +24,12 @@
 	echo '値が入っていません';
 	}
 
-	
-	
-/*	
-	if(!isset($_POST['select_j'])) {
-	$select_j_id = $_SESSION['select_j'];
-	}else{
-	$select_j_id = $_POST['select_j'];
-	$_SESSION['select_j'] =$select_j_id;
-	}
-	
-*/	
-
-
-	
 	require_once('include_path.php');
 	require_once('db.php');
 	$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
 
 	
 	printf($k);printf($k);printf($k);
-	
-	printf($k);printf($k);
-	
 	
 	$mj_list_query = "select * from mj_list where j_id = " . $select_j_id; // . "order by m_id"
 //	選択されたジャンルに参加する人のm_idを取得する
@@ -76,51 +54,30 @@
 		}
 		$k = "<br/>";
 		
-		$query = "select * from member,mj_list,janru where member.m_id =".$m_id." and mj_list.m_id = member.m_id and mj_list.j_id = janru.j_id order by mj_list.votes desc"; //とってきたジャンルで選択されたmemberを一人ずつ表示
+		$query = "select * from member,mj_list,janru where member.m_id =".$m_id." and mj_list.m_id = member.m_id and mj_list.j_id = janru.j_id order by mj_list.votes desc limit 3"; //とってきたジャンルで選択されたmemberを一人ずつ表示
 		$result = $dbc -> query($query);
-					
+		
+		$i =0;
+		
 		while($row = $result -> fetch_assoc()) {
 			
+			//順位の表示
+			$i = $i + 1;
+			echo '第'.$i.'位';
 			
-			$bun1 = "参加者ID:%d "; // ""内はHTML
-			$img_m_id = $row['m_id'];
-			printf($bun1,$img_m_id);
-			printf($k);
-			
+			//参加者情報			
 			$bun2 = "参加者名:%s ";
 			$m_name = $row['name'];
 			printf($bun2,$m_name);
 			printf($k);
-
-
-
-//			$img_query = "SELECT IMG FROM member WHERE m_id = " . $m_id ;
-//			$img_result = $mysqli -> query($img_query);
-//			$img_row = $img_result -> fetch_row();
-//			echo "<img src=" . $img_row[0] . ">";
-//			printf($img);
-//			printf($k);
-//			→直接表示すると文字化けする。
-
-
-			require('imgget.php');		
-		
+			
+			//画像の呼び出し
+			require('imgget.php');
 		
 			$bun3 = "参加者紹介文:%s";
 			$free = $row['free'];
 			printf($bun3,$free);
 			printf($k);
-			
-			
-	/*		$vquery = "select * from mj_list where m_id =". $m_id . " and j_id = " . $_SESSION['select_j'];
-			$vresult = $dbc -> query($vquery);
-			while($vote_row = $vresult -> fetch_assoc()) {
-			
-				$total = $vote_row['votes'];
-				printf("<br />"."得票数: %d \n", $total);
-					
-			}
-	*/		
 			
 			printf($k);printf($k);printf($k);printf($k);printf($k);
 			printf("----------------------------------------------------------------------------------------------");
@@ -133,15 +90,6 @@
 	printf($k);printf($k);printf($k);
 	printf($k);printf($k);printf($k);
 	
-	
-	
-
-	
-		
-		
-	
-//	unset($_SESSION['img_id']);
-
 ?>
 
 <html>

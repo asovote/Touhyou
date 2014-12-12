16d20c6704a4262060fb522d2753f9f571de65c8
hayashi <st1201515@yahoo.co.jp>
2014-12-12 12:14:46 +0900

    no message

--- aso/view/profile_update.php
+++ aso/view/profile_update.php
@@ -73,8 +73,9 @@ return str_replace($search,$replace,$str);
 			}
 			
 			echo '</select></p>';
-		
-			echo '<p>フリー： ※300文字以内</p><textarea name="free" cols="30" rows="5">'.$str = h($mfree);$str = tag_kyoka($mfree);echo $str2;.'</textarea>';
+			$str = h($mfree);
+			$str = tag_kyoka($mfree);
+			echo '<p>フリー： ※300文字以内</p><textarea name="free" cols="30" rows="5">'.$str.'</textarea>';
 			
 			echo '<p>写真：<input type="file" name="upfile" size="30"/></p>';
 			

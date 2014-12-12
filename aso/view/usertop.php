ab28a11932968ced0a23b0975fcd4030b8665df0
1201519 <st1201515@yahoo.co.jp>
2014-12-12 12:18:05 +0900

    no message

--- aso/view/usertop.php
+++ aso/view/usertop.php
@@ -1,4 +1,4 @@
-﻿<!doctype html>
+<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -18,75 +18,56 @@
 
 </head>
 
-<body style="background-image":url(背景2.png);background-attachment:fixed; class="img">
-
-
-
-	<?php
+<body>
+<body style="background-image:url(img/123.jpg);background-attachment:fixed;">
+<?php
 		//データベースに接続
 	//	ini_set('include_path', '/xampp/htdocs/aso/classes/');
 		$classDir = __DIR__ . "/../class/";
 		//require_once('include_path.php');
 		require_once($classDir . 'db.php');
 		require_once($classDir . 'session_start.php');
-		unset($_SESSION['jid']);
 		
-		setcookie("use_cookie",'true',time()+60*60*24*1);
+		echo '<p class="head"><img src="img/2222.png"alt=""/></p>';
+		echo '<p class="rr"><img src="img/rr.png"alt=""/></p>';
+		echo '<div id="btb2" align="center"></div>';	
+			
 		
-		// Cookieが有効でない場合
-		if(!isset($_COOKIE['use_cookie'])){
-		    echo '投票は、Cookieを有効にする必要があります。';
-		}else{		
-		
-		echo '<div class="heading07"><p1>投票ページ</p1></div>';
 		
 		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
 		$query = "select * from janru where jtime ='0';";
 		$result = mysqli_query($dbc, $query);
 		
+		
+			
+			
+		/* 投票ページへ */			
 		while($row = mysqli_fetch_array($result)) {
-		echo '<div class= "menu4">';
-			
-			echo '<name="janru">';	
-				$janru_id = $row['j_id'];
-				$j_name = $row['j_name'];
-		echo '<value='.$janru_id.'>';
-		echo '<form action="data.php" method="POST">';
-		echo '<div align="center"><input id="subbtn" type="submit" name="jname" value="'.$j_name.'"></div>';
-		echo '<input id="subbtn" type="hidden" name="jid" value="'.$janru_id.'">';
-		echo '</div></div></div>';
-		echo '</form>';
-		}
-		
-		
-		
-		$dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
-		$query = "select * from janru where jtime ='2';";
-		$result = mysqli_query($dbc, $query);
-		
-		if(mysqli_fetch_row($result) != 0){
-		
-			echo '<div class="heading07"><p1>投票結果</p1></div>';
-			
-			mysqli_data_seek($result,0);
-			
-			while($row = mysqli_fetch_array($result)) {
 			echo '<div class= "menu4">';
 				
 				echo '<name="janru">';	
-					$janru_id = $row['j_id'];
-					$j_name = $row['j_name'];
-			echo '<value='.$janru_id.'>';
-			echo '<form action="user_result.php" method="POST">';
-			echo '<div align="center"><input id="subbtn" type="submit" name="jname" value="'.$j_name.'"></div>';
-			echo '<input id="subbtn" type="hidden" name="jid" value="'.$janru_id.'">';
-			echo '</div></div></div>';
+						$janru_id = $row['j_id'];
+						$j_name = $row['j_name'];
+				echo '<value='.$janru_id.'>';
+				echo '<form action="data.php" method="POST">';
+				echo '<p class="bt">';
+					echo '<a>'.$j_name.'</a>';
+					echo '<right><img src="img/noun_63654_cc.png" width="20" height="20"  alt=""/></right>';
+				echo '</p>';
+				echo '<input id="subbtn" type="hidden" name="jid" value="'.$janru_id.'">';
+			echo '</div';
 			echo '</form>';
-			}
 		}
+
+
+		/* 結果発表ページへ */
+       
+			
 		
-	}
 	?>
+
+
+	
         
 </body>
 </html>

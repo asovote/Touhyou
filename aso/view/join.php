c291f017853932b808cf91e0669bda773ee9cb6c
Nakano <enqty0717@gmail.com>
2014-12-15 05:16:14 +0900

    new file join_post

--- aso/view/join.php
+++ aso/view/join.php
@@ -3,16 +3,11 @@
 	
 	require_once('include_path.php');
 	require_once('db.php');
-	$flag = 0;
 $dsn = 'mysql:dbname='.db_name.';host='.db_host.'';
 
 try{
     $dbh = new PDO($dsn,db_user,db_pass);
 
-	//$dbh->query('SET NAMES utf8');
-
-//$genre = 15;
-//$genre2 = 15;
 ?>
 
 <!doctype html>
@@ -54,25 +49,6 @@ while($result = $stmt -> fetch(PDO::FETCH_ASSOC)){
 </select>
 <input type="submit">
 </form>
-<?php
-if($flag == 1){
-$sql = 'SELECT mj_list.m_id,member.name,mj_list.j_id,janru.j_name as genre FROM touhyou.mj_list,touhyou.member,touhyou.janru where mj_list.m_id = member.m_id
-and mj_list.j_id = janru.j_id and mj_list.j_id in (?,?) order by votes desc';
-$stmt = $dbh->prepare($sql);
-//$stmt -> execute(array(15,15));
-$genre = $_POST['genre1'];
-$genre2 = $_POST['genre2'];
-$stmt -> execute(array($genre,$genre2));
-  while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-        print($result['m_id']);
-        print($result['name']);
-		print($result['j_id']);
-		print($result['genre']);
-		print('<BR>');
-    }  
-}
-
-?>
 </html>
 <?php
 }catch (PDOException $e){
---
+++ aso/view/join_post.php
@@ -0,0 +1,46 @@
+<?php
+
+	
+	require_once('include_path.php');
+	require_once('db.php');
+$dsn = 'mysql:dbname='.db_name.';host='.db_host.'';
+
+try{
+    $dbh = new PDO($dsn,db_user,db_pass);
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<?php
+if(ini_set($_POST['genre1'])){
+$sql = 'SELECT mj_list.m_id,member.name,mj_list.j_id,janru.j_name as genre FROM touhyou.mj_list,touhyou.member,touhyou.janru where mj_list.m_id = member.m_id
+and mj_list.j_id = janru.j_id and mj_list.j_id in (?,?) order by votes desc';
+$stmt = $dbh->prepare($sql);
+//$stmt -> execute(array(15,15));
+$genre = $_POST['genre1'];
+$genre2 = $_POST['genre2'];
+$stmt -> execute(array($genre,$genre2));
+  while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        print($result['m_id']);
+        print($result['name']);
+		print($result['j_id']);
+		print($result['genre']);
+		print('<BR>');
+    }  
+}
+
+?>
+
+
+<body>
+</body>
+</html>
+<?php
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
+?>

3e390457bef6364b188678aa146be00560fa1271
れがろ <st1201515@yahoo.co.jp>
2014-12-15 00:26:32 +0900

    no message

--- aso/view/usertop.php
+++ aso/view/usertop.php
@@ -100,7 +100,8 @@
 					$j_name = $row['j_name'];
 			echo '<value='.$janru_id.'>';
 			echo '<form action="user_result.php" method="POST">';
-			echo '<div align="center"><input id="subbtn" type="submit" name="jname" value="'.$j_name.'"></div>';				echo '<p class="bt">';
+			echo '<div align="center"><input id="subbtn" type="submit" name="jname" value="'.$j_name.'"></div>';	
+　　　　　　　　　　　　echo '<p class="bt">';
 			echo '<a>'.$j_name.'</a>';
 			echo '<right><img src="img/noun_63654_cc.png" width="20" height="20"  alt="user_result.php"/></right>';
 		　　　　echo '</p>';

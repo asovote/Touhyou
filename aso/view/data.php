fb971b3cda5dc12d3ce5986aa964fb66ff187630
れがろ <st1201515@yahoo.co.jp>
2014-12-15 03:57:05 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -115,7 +115,7 @@ if(isset($_COOKIE[$jid])){
 		$result = mysqli_query($dbc, $query);
 	       
 	       
-	       
+	       	echo '<div id="btb2" align="center"></div>';
 		while($row = mysqli_fetch_array($result)){
 			
 			//表示処理
@@ -129,9 +129,10 @@ if(isset($_COOKIE[$jid])){
       		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';
-		  echo'<div align="center" valign="bottom"><input type="image" src="img/vote.png"width="20" height="30" ><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form><br>';
+		  echo'<div align="center" valign="bottom"><input type="image" src="img/vote.png"width="50" height="50" ><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form><br>';
                   echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
 		  echo'</div>';
+		  echo '<div id="btb2" align="center"></div>';
 		}	 
 }
 ?>   </div> </body>

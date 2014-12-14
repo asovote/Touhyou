f56e79d366a4903afbcc7122d53114d11a2fcfdb
れがろ <st1201515@yahoo.co.jp>
2014-12-15 01:32:19 +0900

    no message

--- aso/view/usertop.php
+++ aso/view/usertop.php
@@ -102,7 +102,7 @@
 			echo '<form action="user_result.php" method="POST">';
 			echo '<p class="bt">';
 			echo '<a>'.$j_name.'</a>';
-			echo '<right><img src="img/noun_63654_cc.png" width="20" height="20"  alt="data.php"/></right>';
+			echo '<right><input type="image" src="img/noun_63654_cc.png" width="20" height="20"  alt=""/></right>';
 			echo '</p>';
 			echo '<input id="subbtn" type="hidden" name="jid" value="'.$janru_id.'">';
 			echo '</div></div></div>';

193e2eb98450df778f6ac8335aec36961ff0606c
Nakano <enqty0717@gmail.com>
2014-12-15 03:49:39 +0900

    no message

--- aso/view/join.php
+++ aso/view/join.php
@@ -4,13 +4,11 @@
 	require_once('include_path.php');
 	require_once('db.php');
 $dsn = 'mysql:dbname='.db_name.';host='.db_host.'';
-$user = 'root';
-$password = 'lovelive';
 
 try{
     $dbh = new PDO($dsn,db_user,db_pass);
 
-	$dbh->query('SET NAMES utf8');
+	//$dbh->query('SET NAMES utf8');
 
     $sql = 'SELECT mj_list.m_id,member.name,mj_list.j_id,janru.j_name as genre FROM touhyou.mj_list,touhyou.member,touhyou.janru where mj_list.m_id = member.m_id
 and mj_list.j_id = janru.j_id and janru.j_id = ? or mj_list.j_id = janru.j_id and janru.j_id = ? ';

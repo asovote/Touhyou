fb5b2fec456edbc90291efc4eb18c65b3b03a08b
Nakano <enqty0717@gmail.com>
2014-12-15 03:37:45 +0900

    new file

---
+++ aso/view/join.php
@@ -0,0 +1,62 @@
+<?php
+
+	require_once('file:///C|/xampp/htdocs/aso2/view/include_path.php');
+	require_once('file:///C|/xampp/htdocs/aso2/view/db.php');
+$dsn = 'mysql:dbname='.db_name.';host='.db_host.'';
+$user = 'root';
+$password = 'lovelive';
+
+try{
+    $dbh = new PDO($dsn,db_user,db_pass);
+
+	$dbh->query('SET NAMES utf8');
+
+    $sql = 'SELECT mj_list.m_id,member.name,mj_list.j_id,janru.j_name as genre FROM touhyou.mj_list,touhyou.member,touhyou.janru where mj_list.m_id = member.m_id
+and mj_list.j_id = janru.j_id and janru.j_id = ? or mj_list.j_id = janru.j_id and janru.j_id = ? ';
+
+//$genre = 15;
+//$genre2 = 15;
+$stmt = $dbh->prepare($sql);
+$stmt -> execute(array(15,15));
+  if($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        print($result['m_id']);
+        print($result['name']);
+		print($result['j_id']);
+		print($result['genre']);
+    }  
+
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+
+<form method="post" action="#">
+
+<select name="genre1" size="3" multiple>
+<?php
+
+echo "<option value=\"\">サンプル1</option>";
+
+?>
+
+</select>
+<select name="genre2" size="3" multiple>
+<?php
+
+
+
+?>
+
+</select>
+
+</form>
+</html>

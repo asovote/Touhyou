6763452f344fcafe0bb5f2e8e7d3c536d019c996
れがろ <st1201515@yahoo.co.jp>
2014-12-15 02:56:48 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -125,9 +125,10 @@ if(isset($_COOKIE[$jid])){
  			$mimg = $row['m_img'];
 			$_SESSION['jid'] = $jid;
          	  echo'<div class="row">';
+		  echo'<h3>'.$mname.'</h3>';
       		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';
-		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">'; echo'<h3>'.$mname.'</h3>';
+		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';
 		  echo'<div align="center" valign="bottom"><input type="submit"value="投票" "><input type="hidden" name="mid" value="'.$mid.'"><input type="hidden" name="jid" value="'.$jid.'"></form><br>';
                   echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';
 		  echo'</div>';

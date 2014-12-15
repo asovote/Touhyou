658e9a450d70af521fcb6a3b51ccc32c3dff096a
1201503 <st1201515@yahoo.co.jp>
2014-12-15 15:29:47 +0900

    no message

--- aso/view/profile_update.php
+++ aso/view/profile_update.php
@@ -64,7 +64,7 @@ return str_replace($search,$replace,$str);
 			echo '<form action="janru_top.php?up=1" method="POST" enctype="multipart/form-data">';
 			echo '<p>氏名：<input type="text" name="name" value="'.$mname.'"/></p>';
 			echo '<p>学校：<input type="text" name="school" value="'.$mschool.'"/></p>';
-			echo '<p>ジャンル：<select name="janru" value="'.$jname.$mimg'">';
+			echo '<p>ジャンル：<select name="janru" value="'.$jname."".$mimg.'">';
 		
 			while($row = mysqli_fetch_array($result)) {
 				$janru_id = $row['j_id'];

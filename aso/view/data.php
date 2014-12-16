7400094676ab0465fbc556642daab1dc2c4b0cc1
Kurogi <st1201515@yahoo.co.jp>
2014-12-16 14:05:20 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -89,7 +89,7 @@ if(isset($_COOKIE[$jid])){
          	  echo'<div class="row">';
 		  echo'<h4><b>'.$mname.'</b></h4>';
 		  echo '<form action="update.php" method="POST">';
-      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';//<a title="Image 1" href="un_profile_select.php?mid=' .$mid.'">
+      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="un_profile_select.php?mid=' .$mid.'">';
 		  echo'<img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';
 		  echo'<div align="center" valign="bottom">';
@@ -119,8 +119,7 @@ if(isset($_COOKIE[$jid])){
 			$_SESSION['jid'] = $jid;
          	  echo'<div class="row">';
 		  echo'<h3><b>'.$mname.'</b></h3>';
-      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';
-		  //<a title="Image 1" href="u_profile_select.php?mid=' .$mid.'">
+      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="u_profile_select.php?mid=' .$mid.'">';
 		  echo'<img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';

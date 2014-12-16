00343c370001cf0e94f0f309f1c472ddd3099507
我はオウガイ <tuvait_fiaret@yahoo.co.jp>
2014-12-17 03:40:26 +0900

    no message

--- aso/view/janru_add.php
+++ aso/view/janru_add.php
@@ -1,1 +1,31 @@
-﻿
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ジャンルの編集</title>
+<link type="text/css" rel="stylesheet" href="">
+</head>
+
+<body>
+<?php
+
+session_start();
+
+if($_SESSION['ad_id'] == null){	
+header('Location: /ad_login.php');
+}
+
+
+
+$jname = $_POST["jname"];
+echo $jname;
+$query = "INSERT INTO janru(j_id, j_name) VALUES (null,'$jname');";
+
+
+
+
+?>
+</body>
+</html>

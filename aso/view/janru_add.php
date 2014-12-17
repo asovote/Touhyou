b7aceff4d22138a5e80c92a71956ff603ccaf392
Nakano <enqty0717@gmail.com>
2014-12-18 00:12:08 +0900

    no message

--- aso/view/janru_add.php
+++ aso/view/janru_add.php
@@ -36,7 +36,9 @@ while($chk_row = $chkresult -> fetch_array()){
 }
 
 if($err == 0){
-	header('Location: /janru_insert.php?err=0');			//ジャンル名未入力時
+	print($_POST['jname']);
+	foreach($_POST as $idx => $val){echo "$idx = $val<br>";}
+	//header('Location: /janru_insert.php?err=0');			//ジャンル名未入力時
 }else if($err == 1){
 	header('Location: /janru_insert.php?err=1');		//重複
 }else{

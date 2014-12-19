6bef09f893302585f7e1657b1965fdbb3e0e3088
Kurogi <st1201515@yahoo.co.jp>
2014-12-19 12:07:59 +0900

    no message

--- aso/view/VoteAdmin/php/DbProfile.php
+++ aso/view/VoteAdmin/php/DbProfile.php
@@ -1,5 +1,5 @@
 <?php
   define('dbAccount', 'admin');
   define('dbPassword', 'adminadmin');
-  define('dbDsn', "mysql:dbname=touhyou;host=localhost;");
+  define('dbDsn', "mysql:dbname=touhyou;host=touhyo-db.curxp9mhiepe.ap-northeast-1.rds.amazonaws.com;");
 ?>

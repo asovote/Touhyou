2a8b3efdd644865c64dfc355f83bc199960ae177
1201519 <st1201515@yahoo.co.jp>
2014-12-19 12:51:14 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -24,6 +24,9 @@
 	        return flag;
 	    }
 	</script>
+    <style type="text/css">
+	p.example1 { margin: 50px 10px 50px 200px; }
+	</style>
 	</head>  
     <body topmargin="10px" bottommargin="0" leftmargin="20" rightmargin="20" marginheight="0" marginwidth="20" style="background-image:url(img/123.png);background-attachment:fixed;">
     
@@ -101,8 +104,8 @@ if(isset($_COOKIE[$jid])){
          	  echo'<div class="row">';
 		  echo'<h4><b>'.$mname.'</b></h4>';
 		  echo '<form action="update.php" method="POST">';
-      		  echo'<table><tr><td align="center"><div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="un_profile_select.php?mid=' .$mid.'">';
-		  echo'<img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></a></div><!--SQLで撮ってきた画像に差し替え--></td></tr></table>';
+      		  echo'<div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 1" href="un_profile_select.php?mid=' .$mid.'">';
+		  echo'<p class="example1"><img class="thumbnail img-responsive" src="img/'.$mimg.'"width="600" height="350" ></p></a></div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo'<div class="col-lg-3 col-sm-4 col-xs-6">';
 		  echo'<div align="center" valign="bottom">';
                   echo'</div>  <!--ここで戻るボタンと投票ボタンを置く形になるはずです--></div>';

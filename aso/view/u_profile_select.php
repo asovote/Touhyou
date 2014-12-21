c17a6f02a62e31bdf68a3dc767a225d1406f353e
masaki <st1201519@yahoo.co.jp>
2014-12-22 02:37:59 +0900

    no message

--- aso/view/u_profile_select.php
+++ aso/view/u_profile_select.php
@@ -82,8 +82,8 @@
 				$mimg = $row['m_img'];
 	      echo'<div class="row">';
 	      echo'<h3><b>'.$mname.'</b></h3>';
-	      echo'<div class="col-lg-3 col-sm-4 col-xs-6">
-		  			<img class="thumbnail img-responsive" src="img/'.$mimg.'" width="600" height="350"></a>
+	      echo'<div class="yam">
+		  			<img class="thumbnail img-responsive" src="img/'.$mimg.'" width="600" height="350" ></a>
 				</div><!--SQLで撮ってきた画像に差し替え-->';
 		  echo '<form action="update.php" method="POST" onClick="return submitChk();">';       
 		  echo '<dic class="col-lg-3 col-sm-4 col-xs-6">';

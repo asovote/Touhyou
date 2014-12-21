882afc6cd741652d6b4fbbbdb880c8027ab97b1d
masaki <st1201519@yahoo.co.jp>
2014-12-22 01:04:51 +0900

    no message

--- aso/view/data.php
+++ aso/view/data.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<title>Title</title>
 		<meta name="generator" content="Bootply" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -25,6 +25,7 @@
 	    }
 	</script>
 	</head>  
+    <div id="container">
     <body style="background-image:url(img/123.png);background-attachment:fixed;">
     
 

354543a39ae66564a4b4bce4f74d604fa977e0b3
Nakano <enqty0717@gmail.com>
2014-12-22 01:51:44 +0900

    no message

--- aso/view/profile_insert.php
+++ aso/view/profile_insert.php
@@ -98,7 +98,7 @@
 			}else{
 		
 				if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {	
-				echo $_FILES["upfile"]["tmp_name"];
+				echo $_FILES["upfile"]["name"];
 				echo pathinfo($_FILES["upfile"]["tmp_name"]);
 
 	  			if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "img/".$_FILES["upfile"]["name"])) {

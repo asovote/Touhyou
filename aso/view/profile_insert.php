c39e73e82ddfd10724a711323e25fd8f72f1b0d7
Nakano <enqty0717@gmail.com>
2014-12-22 01:50:59 +0900

    no message

--- aso/view/profile_insert.php
+++ aso/view/profile_insert.php
@@ -102,7 +102,8 @@
 				echo pathinfo($_FILES["upfile"]["tmp_name"]);
 
 	  			if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "img/".$_FILES["upfile"]["name"])) {
-	    			chmod("img/" . $_FILES["upfile"]["name"], 0644);
+	    			echo $_FILES["upfile"]["name"];
+				chmod("img/" . $_FILES["upfile"]["name"], 0644);
 			//	header("Location: janru_top.php");
 //	  			if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "img/". $_FILES["upfile"]["name"])) {
 				} else {
